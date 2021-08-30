f6f8369c31e35be46b7b91aba1cdbc1c5705ea47
Sagar Murlidhar Mhaske <86242988+sagarmhaske7088@users.noreply.github.com>
2021-08-30 10:18:31 +0530

    Add files via upload

---
+++ Testthread.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Testthread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(Thread.currentThread());
+System.out.println(Thread.currentThread().getName());
+System.out.println(Thread.currentThread().getId());
+
+	}
+
+}
